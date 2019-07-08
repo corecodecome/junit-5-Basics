17b1acf211d1e16b9e05a0f7f9760470263999e4
Oleg <jjscorecodecome@protonmail.com>
2019-07-08 17:43:29 +0300

    In TestClassCommitAndPus - Added "part 2"

--- junit-5-basics/src/main/java/io/javabrains/TestClassCommitAndPush.java
+++ junit-5-basics/src/main/java/io/javabrains/TestClassCommitAndPush.java
@@ -5,6 +5,7 @@ public class TestClassCommitAndPush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello there!:)");
+		System.out.println("Hello there!:) part 2");
 
 	}
 
